0fc6098993cbdbe1e7f3b96e1c4c05d47f439c2b
Wilson Gramer <wilson@gramer.dev>
2021-04-26 13:49:46 -0400

    Don't use cross on macOS [skip ci]

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 target=$1
+cross=$2
 
 mkdir -p bin
 
-cargo install cross
-cross build --target=$target --release
+if [ "$cross" = "true" ]; then
+    cargo install cross
+    cross build --target=$target --release
 
-cp target/$target/release/wipple_bundled_interpreter bin/wipple-bundled-$target
-cp target/$target/release/wipple_cli bin/wipple-cli-$target
+    cp target/$target/release/wipple_bundled_interpreter bin/wipple-bundled-$target
+    cp target/$target/release/wipple_cli bin/wipple-cli-$target
+else
+    cargo build --release
+
+    cp target/release/wipple_bundled_interpreter bin/wipple-bundled-$target
+    cp target/release/wipple_cli bin/wipple-cli-$target
+fi
