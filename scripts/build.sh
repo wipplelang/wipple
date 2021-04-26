a15ece4c7d3113570c3f7e89b40f8b14425b1a41
Wilson Gramer <wilson@gramer.dev>
2021-04-26 13:34:38 -0400

    Parallelize builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-case $1 in
-    macos)
-        TARGETS=(
-            "x86_64-apple-darwin"
-        )
-        ;;
-    linux)
-        TARGETS=(
-            "i686-unknown-linux-gnu"
-            "x86_64-unknown-linux-gnu"
-            "armv7-unknown-linux-gnueabihf"
-            "aarch64-unknown-linux-gnu"
-        )
-        ;;
-esac
+target=$1
 
 mkdir -p bin
 
 cargo install cross
+cross build --target=$target --release
 
-for target in ${TARGETS[@]}; do
-    cross build --target=$target --release
-    cp target/$target/release/wipple_bundled_interpreter bin/wipple-bundled-$target
-    cp target/$target/release/wipple_cli bin/wipple-cli-$target
-done
+cp target/$target/release/wipple_bundled_interpreter bin/wipple-bundled-$target
+cp target/$target/release/wipple_cli bin/wipple-cli-$target
