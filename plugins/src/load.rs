b93f97e14da23da2790c8e67795d5b8b48d7154a
Wilson Gramer <wilson@gramer.dev>
2021-04-27 13:00:26 -0400

    Properly keep plugin libraries loaded

--- plugins/src/load.rs
+++ plugins/src/load.rs
@@ -1,23 +1,27 @@
 #![cfg(not(target_arch = "wasm32"))]
 
 use dlopen::symbor::Library;
+use std::result::Result;
 use wipple::*;
 
-pub type PluginFn = extern "C" fn(&EnvironmentRef, &Stack) -> Box<Result>;
+pub type PluginFn = extern "C" fn(&EnvironmentRef, &Stack) -> Box<wipple::Result>;
+
+ref_thread_local! {
+    // Libraries will be kept loaded until the program terminates
+    static managed LOADED_LIBRARIES: Vec<Library> = Vec::new();
+}
 
 pub fn load_plugin(
     path: std::path::PathBuf,
-    env: &wipple::EnvironmentRef,
-    stack: &wipple::Stack,
+    env: &EnvironmentRef,
+    stack: &Stack,
 ) -> wipple::Result {
-    let convert_error =
-        |error| wipple::Return::error(&format!("Error loading plugin: {}", error), &stack);
+    let convert_error = |error| Return::error(&format!("Error loading plugin: {}", error), &stack);
 
     let lib = Library::open(path).map_err(convert_error)?;
     let plugin: PluginFn = *unsafe { lib.symbol("_wipple_plugin") }.map_err(convert_error)?;
 
-    // HACK: keep the library loaded for now
-    std::mem::forget(lib);
+    LOADED_LIBRARIES.borrow_mut().push(lib);
 
     // SAFETY: Need to bind to a variable to prevent premature deallocation of
     // the returned result
