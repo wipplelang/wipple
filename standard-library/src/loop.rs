32b124fc9fb10911e8bef752a4ab2fb65aab0c7a
Wilson Gramer <wilson@gramer.dev>
2021-04-28 13:17:54 -0400

    Make 'loop' accept any value, not just blocks

--- standard-library/src/loop.rs
+++ standard-library/src/loop.rs
@@ -3,17 +3,13 @@ use crate::*;
 pub fn setup(env: &EnvironmentRef) {
     env.borrow_mut().set_variable(
         "loop",
-        Value::of(Function::new(|value, env, stack| {
-            let block = value.get_or::<Block>("Expected block", env, stack)?;
-
-            loop {
-                match block.do_inline(env, stack) {
-                    Ok(_) => {}
-                    Err(r#return) => {
-                        return match r#return {
-                            Return::Break(value, _) => Ok(value),
-                            _ => Err(r#return),
-                        }
+        Value::of(Function::new(|value, env, stack| loop {
+            match value.evaluate(env, stack) {
+                Ok(_) => {}
+                Err(r#return) => {
+                    return match r#return {
+                        Return::Break(value, _) => Ok(value),
+                        _ => Err(r#return),
                     }
                 }
             }
