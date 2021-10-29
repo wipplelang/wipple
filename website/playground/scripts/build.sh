b9680bd83b3dd53d98f5c193812a661244382129
Wilson Gramer <wilson@gramer.dev>
2021-10-28 23:29:12 -0400

    Update Netlify config

---
+++ website/playground/scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+if ! [[ -z $CI ]]; then
+    rustup toolchain install stable
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+
+(cd runner && wasm-pack build)
+
+npm run build
+npm run export
