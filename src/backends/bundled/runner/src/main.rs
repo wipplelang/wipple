e2b4586563013898f52cfb6f74ea6371f70d621e
Wilson Gramer <wilson@gramer.dev>
2021-12-30 19:20:50 -0500

    Improve performance of bundled runner

--- src/backends/bundled/runner/src/main.rs
+++ src/backends/bundled/runner/src/main.rs
@@ -1,19 +1,37 @@
-use std::{env, fs, io::Read, mem};
+use std::{
+    env, fs,
+    io::{self, Read, Seek},
+    mem,
+};
 
 fn main() {
     let bin_path = env::current_exe().expect("Could not locate current executable");
 
-    let bin = fs::File::open(bin_path)
-        .and_then(|file| file.bytes().collect::<Result<Vec<_>, _>>())
-        .expect("Could not open current executable");
+    let mut file = fs::File::open(bin_path).expect("Could not open current executable");
 
-    let item_size_index = bin.len() - mem::size_of::<u64>();
-    let item_size = u64::from_ne_bytes(bin[item_size_index..].try_into().unwrap()) as usize;
+    let item_size = (|| {
+        file.seek(io::SeekFrom::End(-(mem::size_of::<u64>() as i64)))?;
 
-    let end_index = bin.len() - mem::size_of::<u64>();
-    let item = &bin[end_index - item_size..end_index];
+        let mut buf = [0u8; mem::size_of::<u64>()];
+        file.read_exact(&mut buf)?;
 
-    let item = bincode::deserialize(item).expect("Invalid binary");
+        io::Result::Ok(u64::from_ne_bytes(buf))
+    })()
+    .expect("Could not determine bundle location in executable");
+
+    let bundle = (|| {
+        file.seek(io::SeekFrom::End(
+            -(item_size as i64) - mem::size_of::<u64>() as i64,
+        ))?;
+
+        let mut buf = vec![0; item_size as usize];
+        file.read_exact(&mut buf)?;
+
+        io::Result::Ok(buf)
+    })()
+    .expect("Could not read bundle");
+
+    let item = bincode::deserialize_from(bundle.as_slice()).expect("Invalid bundle");
 
     wipple_interpreter_backend::set_output(|text| println!("{}", text));
 
