ea48d2e5b727ec7dc3149f59c2173803800ef0cf
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2022-06-23 15:26:28 -0400

    Remove old playground

--- website/playground-old/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import { AppProps } from "next/app";
-import "../styles/globals.css";
-
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
-
-export default App;
--- website/playground-old/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-"$(dirname $(realpath $0))/wasm.sh"
-npm install
-npm run build
-npm run export
