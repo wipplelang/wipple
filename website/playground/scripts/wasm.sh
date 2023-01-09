c58ba1708d446e93b0ada5446f71cb68e38e1e06
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2023-01-09 16:01:04 -0500

    Use -O1 instead of -Os on playground

--- website/playground/scripts/wasm.sh
+++ website/playground/scripts/wasm.sh
@@ -4,7 +4,7 @@ set -e
 
 if ! [[ -z $CI ]]; then
     rustup toolchain install stable
-    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -f
 fi
 
 if ! [[ -z $CI ]]; then
