1e1d0639e362d126811de4bc85eeddf897b50eda
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2023-02-26 13:44:22 -0500

    Add PWA config

--- website/playground/vite.config.ts
+++ website/playground/vite.config.ts
@@ -2,9 +2,42 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [react(), wasm(), topLevelAwait()],
+    plugins: [
+        react(),
+        wasm(),
+        topLevelAwait(),
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true,
+            },
+            includeAssets: ["images/**/*"],
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,wasm,json}"],
+                maximumFileSizeToCacheInBytes: 10 * 1024 ** 2, // 10 MB
+            },
+            manifest: {
+                name: "Wipple Playground",
+                short_name: "Wipple",
+                description: "Learn and experiment with the Wipple programming language",
+                icons: [
+                    {
+                        src: "./images/logo-192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "./images/logo-512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
     worker: {
         format: "es",
         plugins: [wasm(), topLevelAwait()],
@@ -12,5 +45,5 @@ export default defineConfig({
     server: {
         port: 3000,
     },
-    base: process.env.NODE_ENV === "development" ? undefined : "/playground",
+    base: "/playground",
 });
