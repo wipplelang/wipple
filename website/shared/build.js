73586f8cfcb5b3ae366fe1a99cba7a24ea199298
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2023-07-29 20:52:56 -0400

    (hack) Always build playground in release mode to fix OOM error

--- website/shared/build.js
+++ website/shared/build.js
@@ -1,4 +1,5 @@
 export const rustOptions = (env) => ({
+    debug: false, // HACK: OOM error when building in debug mode
     cargoArgs: env.CI ? [] : ["--features", "debug_playground"],
     wasmOptArgs: ["-O1"],
     verbose: true,
