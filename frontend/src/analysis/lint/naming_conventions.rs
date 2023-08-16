2d2a947393823eeabc76427ea8dffd960ea3f8ae
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2023-08-16 11:58:14 -0400

    Allow acronyms in naming conventions lint

--- frontend/src/analysis/lint/naming_conventions.rs
+++ frontend/src/analysis/lint/naming_conventions.rs
@@ -1,13 +1,37 @@
 use crate::{analysis, diagnostics::*, Compiler};
-use convert_case::{Case, Casing};
+use itertools::Itertools;
 
 impl Compiler {
     pub(super) fn naming_conventions_lint(&self, program: &analysis::Program) {
         // Syntax definitions are not checked here because they often use symbols
 
+        fn convert_case(s: &str, capitalize: bool) -> String {
+            let boundaries = [
+                convert_case::Boundary::Acronym,
+                convert_case::Boundary::Hyphen,
+                convert_case::Boundary::LowerUpper,
+            ];
+
+            convert_case::split(s, &boundaries)
+                .into_iter()
+                .map(|segment| {
+                    if capitalize {
+                        if segment.to_uppercase() == segment {
+                            segment
+                        } else {
+                            let (head, tail) = segment.split_at(1);
+                            head.to_uppercase() + tail
+                        }
+                    } else {
+                        segment.to_lowercase()
+                    }
+                })
+                .join("-")
+        }
+
         for ty in program.declarations.types.values() {
             let name = ty.name.as_str();
-            let correct = name.to_case(Case::Train);
+            let correct = convert_case(name, true);
 
             if name != correct {
                 self.add_diagnostic(
@@ -30,7 +54,7 @@ impl Compiler {
 
         for tr in program.declarations.traits.values() {
             let name = tr.name.as_str();
-            let correct = name.to_case(Case::Train);
+            let correct = convert_case(name, true);
 
             if name != correct {
                 self.add_diagnostic(
@@ -55,7 +79,7 @@ impl Compiler {
             let name = constant.name.as_str();
 
             if constant.is_variant {
-                let correct = name.to_case(Case::Train);
+                let correct = convert_case(name, true);
 
                 if name != correct {
                     self.add_diagnostic(
@@ -75,7 +99,7 @@ impl Compiler {
                     );
                 }
             } else {
-                let correct = name.to_case(Case::Kebab);
+                let correct = convert_case(name, false);
 
                 if name != correct {
                     self.add_diagnostic(
@@ -100,7 +124,7 @@ impl Compiler {
         for param in program.declarations.type_parameters.values() {
             if let Some(name) = param.name {
                 let name = name.as_str();
-                let correct = name.to_case(Case::Train);
+                let correct = convert_case(name, true);
 
                 if name != correct {
                     self.add_diagnostic(
@@ -125,7 +149,7 @@ impl Compiler {
         for variable in program.declarations.variables.values() {
             if let Some(name) = variable.name {
                 let name = name.as_str();
-                let correct = name.to_case(Case::Kebab);
+                let correct = convert_case(name, false);
 
                 if name != correct {
                     self.add_diagnostic(
