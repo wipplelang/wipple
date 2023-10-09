aac3750a63bb05d6afedaeba421b18f485a68505
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2023-10-08 21:41:01 -0400

    Use installed Wipple CLI for Go tests

--- scripts/test-go.sh
+++ scripts/test-go.sh
@@ -8,8 +8,8 @@ echo -e "Working directory: $tempdir"
 for f in ./tests/*.wpl; do
     echo -e "Testing $f"
 
-    if (cargo run --bin wipple -- compile -f go "$f" > "$tempdir/main.go"); then
-        cargo run --bin wipple -- compile -o "$tempdir/out" "$f"
+    if (wipple compile -f go "$f" > "$tempdir/main.go"); then
+        wipple compile -o "$tempdir/out" "$f"
     fi
 
     "$tempdir/out"
