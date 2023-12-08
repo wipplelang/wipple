0b0bc304f5e6d4c7c94652ecb5d5984e766f439a
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2023-12-07 23:57:44 -0500

    Zip Electron app before uploading

--- website/playground-electron/scripts/build.sh
+++ website/playground-electron/scripts/build.sh
@@ -8,4 +8,6 @@ if [[ -z "$CI" ]]; then
     npx electron-builder build
 else
     npx electron-builder build --mac # TODO: More targets
+    (cd dist/mas && zip -r ../mac.zip *)
+    rm -r dist/mas
 fi
