5f030da9949c18e1571147284cb301cde7ed2bad
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2024-01-24 10:39:32 -0500

    Require '[should compile]' or '[should error]' headers in tests

--- test/src/tests.test.ts
+++ test/src/tests.test.ts
@@ -1,5 +1,5 @@
-import { test, expect } from "bun:test";
 import fs from "fs";
+import fsp from "fs/promises";
 import path from "path";
 import { compile, link } from "wipple-compiler";
 import { evaluate, InterpreterError, IoRequest } from "wipple-interpreter";
@@ -19,14 +19,35 @@ const baseInterface = JSON.parse(
 
 const baseLibrary = JSON.parse(fs.readFileSync(path.join(prefix, "base.wipplelibrary"), "utf8"));
 
-for (const file of fs.readdirSync("tests")) {
+for (let file of fs.readdirSync("tests")) {
     if (path.extname(file) === ".wipple") {
         test(file, async () => {
-            const code = fs.readFileSync(path.join("tests", file), "utf8");
+            const code = await fsp.readFile(path.join("tests", file), "utf8");
+            file = path.join("test/tests", file);
+
+            const header = code.match(/\[([\w\s]+)\]/)?.[1];
+            let shouldCompile: boolean;
+            switch (header) {
+                case "should compile": {
+                    shouldCompile = true;
+                    break;
+                }
+                case "should error": {
+                    shouldCompile = false;
+                    break;
+                }
+                default: {
+                    throw new Error(
+                        "expected test to begin with [should compile] or [should error]"
+                    );
+                }
+            }
 
             const compileResult = compile([{ path: file, code }], baseInterface);
 
             if (compileResult.errors.length > 0) {
+                expect(shouldCompile).toBeFalsy();
+
                 expect({ errors: compileResult.errors }).toMatchSnapshot();
                 return;
             }
@@ -34,9 +55,12 @@ for (const file of fs.readdirSync("tests")) {
             const linkResult = link([compileResult.library, baseLibrary]);
 
             if (linkResult.Err) {
+                expect(shouldCompile).toBeFalsy();
                 expect({ errors: linkResult.Err }).toMatchSnapshot();
                 return;
             }
+
+            expect(shouldCompile).toBeTruthy();
 
             const executable = linkResult.Ok;
 
