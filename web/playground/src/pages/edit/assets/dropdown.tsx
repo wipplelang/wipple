d245eb74dc1118c3192bb294c00b2d1d1e32f0c9
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2024-04-27 10:04:40 -0400

    Fix dropdown placement in Firefox

--- web/playground/src/pages/edit/assets/dropdown.tsx
+++ web/playground/src/pages/edit/assets/dropdown.tsx
@@ -8,54 +8,56 @@ export const DropdownAsset = (props: {
     onChange: (value: string) => void;
 }) => {
     return (
-        <ContextMenuButton
-            disabled={props.disabled}
-            items={[
-                ...props.options.map((option) => ({
-                    title: () => (
-                        <code>
-                            <span
-                                className={`ui-font text-blue-500 ${
-                                    option === props.value ? "" : "opacity-0"
-                                }`}
-                            >
-                                {"✓ "}
-                            </span>
-                            {option}
-                        </code>
-                    ),
-                    onClick: () => props.onChange(option),
-                })),
-                {
-                    title: () => (
-                        <code>
-                            <span
-                                className={`ui-font text-blue-500 ${
-                                    props.value != null && !props.options.includes(props.value)
-                                        ? ""
-                                        : "opacity-0"
-                                }`}
-                            >
-                                {"✓ "}
-                            </span>
+        <div className="inline-block align-bottom">
+            <ContextMenuButton
+                disabled={props.disabled}
+                items={[
+                    ...props.options.map((option) => ({
+                        title: () => (
+                            <code>
+                                <span
+                                    className={`ui-font text-blue-500 ${
+                                        option === props.value ? "" : "opacity-0"
+                                    }`}
+                                >
+                                    {"✓ "}
+                                </span>
+                                {option}
+                            </code>
+                        ),
+                        onClick: () => props.onChange(option),
+                    })),
+                    {
+                        title: () => (
+                            <code>
+                                <span
+                                    className={`ui-font text-blue-500 ${
+                                        props.value != null && !props.options.includes(props.value)
+                                            ? ""
+                                            : "opacity-0"
+                                    }`}
+                                >
+                                    {"✓ "}
+                                </span>
 
-                            <span className="opacity-50">Custom...</span>
-                        </code>
-                    ),
-                    onClick: () => {
-                        const value = prompt("Enter a custom value:");
-                        if (!value) return;
+                                <span className="opacity-50">Custom...</span>
+                            </code>
+                        ),
+                        onClick: () => {
+                            const value = prompt("Enter a custom value:");
+                            if (!value) return;
 
-                        props.onChange(value);
+                            props.onChange(value);
+                        },
                     },
-                },
-            ]}
-        >
-            <div className="flex flex-row items-center justify-center rounded-lg h-[calc(1lh-2pt)] border-2 gap-0.5 pl-1.5 pr-0.5 border-gray-100 dark:border-gray-800 overflow-clip hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
-                <p>{props.value ?? props.options[0]}</p>
+                ]}
+            >
+                <div className="flex flex-row items-center justify-center rounded-lg h-[calc(1lh-2pt)] border-2 gap-0.5 pl-1.5 pr-0.5 border-gray-100 dark:border-gray-800 overflow-clip hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors">
+                    <p>{props.value ?? props.options[0]}</p>
 
-                <MaterialSymbol icon="expand_more" size={20} className="text-blue-500" />
-            </div>
-        </ContextMenuButton>
+                    <MaterialSymbol icon="expand_more" size={20} className="text-blue-500" />
+                </div>
+            </ContextMenuButton>
+        </div>
     );
 };
