043c74c5bbd7235d296932a0cae08abd567cafc5
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2024-08-01 14:50:23 -0400

    Use variable fonts in playground

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -4,9 +4,10 @@ export * from "./edit";
 export * from "./models";
 export * from "./runtimes";
 
-import "@fontsource/inter";
-import "@fontsource/jetbrains-mono";
-import "@fontsource/jetbrains-mono/latin-italic.css";
+import "@fontsource-variable/inter";
+import "@fontsource-variable/inter/wght-italic.css";
+import "@fontsource-variable/jetbrains-mono";
+import "@fontsource-variable/jetbrains-mono/wght-italic.css";
 import "react-material-symbols/rounded";
 import "react-loading-skeleton/dist/skeleton.css";
 import "react-piano/dist/styles.css";
