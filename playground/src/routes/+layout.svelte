f920cc6ad31568aee071f81c84860006ea7e596d
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2025-07-25 08:11:54 -0400

    Add Sentry

--- playground/src/routes/+layout.svelte
+++ playground/src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
     import "$lib/styles/style.css";
+    import * as Sentry from "@sentry/browser";
+    import { PUBLIC_SENTRY_DSN } from "$env/static/public";
 
     const { children } = $props();
+
+    Sentry.init({
+        dsn: PUBLIC_SENTRY_DSN,
+        enabled: import.meta.env.PROD,
+    });
 </script>
 
 <div class="not-printing:h-screen not-printing:w-screen flex flex-col">
