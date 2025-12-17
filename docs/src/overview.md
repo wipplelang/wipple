02b5ad38b0c4be3401906bb8aae784232e49fc41
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2025-12-17 16:04:39 -0500

    Merge docs into Astro site

--- docs/src/overview.md
+++
@@ -1,5 +0,0 @@
-# Overview
-
-Welcome to the Wipple documentation! This book contains a tour of the Wipple programming language designed for people with an existing programming background. If you're a beginner looking to learn to code with Wipple, the [Wipple Playground](https://wipple.org/playground) contains 80+ lessons and guides.
-
-In addition to the tour, this book includes a language reference, guides for advanced users, and documentation for the Wipple compiler.
--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,9 +2,31 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "node:path";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { readFileSync } from "node:fs";
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        rehypePlugins: [
+            rehypeHeadingIds,
+            [
+                rehypeAutolinkHeadings,
+                {
+                    behavior: "wrap",
+                    properties: { class: "no-underline" },
+                },
+            ],
+        ],
+        shikiConfig: {
+            theme: "github-light",
+            langs: [JSON.parse(readFileSync("../vscode/syntaxes/wipple.tmLanguage.json", "utf8"))],
+            langAlias: {
+                wipple: "Wipple",
+            },
+        },
+    },
     vite: {
         plugins: [tailwindcss()],
         resolve: {
