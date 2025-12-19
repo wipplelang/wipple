cdb5988e2b02726fa441126eb3969908fafed575
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2025-12-18 19:45:29 -0500

    Add 'prompt' support in Node runtime

--- compiler/runtime/node-prelude.js
+++ compiler/runtime/node-prelude.js
@@ -1,3 +1,17 @@
 const env = {
     display: console.log,
+    prompt: async (message, submit) => {
+        const rl = require("node:readline/promises").createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        let valid = false;
+        while (!valid) {
+            const input = await rl.question(message + ": ");
+            valid = await submit(input);
+        }
+
+        rl.close();
+    },
 };
