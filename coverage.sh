78a04efa5e5bf5d521b1883b8a6fec0985600d57
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2026-02-07 17:53:49 -0500

    Record code coverage in multiple formats

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,8 @@
+rm -rf coverage && mkdir coverage
 eval "$(cargo llvm-cov show-env --sh 2>/dev/null)"
 cargo llvm-cov clean --workspace
 task test
-cargo llvm-cov report --lcov --output-path lcov.info
+cargo llvm-cov report --lcov --output-path=coverage/lcov.info
+cargo llvm-cov report --json --output-path=coverage/coverage.json
+cargo llvm-cov report --html --output-dir=coverage
+cargo llvm-cov report --output-path=/dev/null --show-missing-lines | grep '/compiler/src/' > coverage/uncovered.txt
