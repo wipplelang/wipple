b56d49a5fb401391e1a2edf8afcedd6832bc172b
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2026-02-11 13:57:02 -0500

    Upload source maps to Sentry

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-    plugins: [tailwindcss(), sveltekit()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            tailwindcss(),
+            sveltekit(),
+            sentryVitePlugin({
+                org: env.SENTRY_ORG,
+                project: env.SENTRY_PROJECT,
+                authToken: env.SENTRY_AUTH_TOKEN,
+            }),
+        ],
+        build: {
+            sourcemap: true,
+        },
+    };
 });
